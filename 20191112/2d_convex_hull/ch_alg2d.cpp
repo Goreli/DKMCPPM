387a1120d3ef9cbcd8f0fbf7d0d4cc7d11087ede
David Krikheli <mindreader@live.com.au>
2019-11-30 14:10:22 +1100

    Added a description of the algorithm at the end of module ch_alg2d.cpp

--- 20191112/2d_convex_hull/ch_alg2d.cpp
+++ 20191112/2d_convex_hull/ch_alg2d.cpp
@@ -71,22 +71,59 @@ void dk::prepareData(DataContainer& dataStore, DataPoint2D& centroid) noexcept {
     // to make sure the algorithm delivers a closed line loop.
     dataStore.push_back(dataStore[0]);
 }
+
+/*
+    Executes multiple passes through the set of input points.
+    By virtue of pre-sorting the very first point is good, so the first
+    pass saves the first point.
+
+    Each pass examines points following the last good one saved and identifies
+    the next good one to save by applying the steps below. Every time it saves
+    a good point it also memorises the respective vector relative to the
+    centroid because it's required for subsequent calculations. We will call
+    this vector the last known good vector.
+
+    The steps are:
+        1.1 Save the good point identified in the previous pass. If this is the
+            first pass than save the first point;
+        1.2 For the good point calculate a vector relative to the centroid.
+            Call it the last known good vector.
+    If there are any points in the input sequence remaining after the last good
+    point saved then for each of them:
+        2.1 Calculate a respective vector relative to the centroid;
+        2.2 Calculate a delta vector between the result of step 2.1 and last
+            known good vector;
+        2.3 Calculate an angle between the delta vector and last known good
+            vector;
+        2.4 Assess the angle. If it's smaller than any of the previous
+            angles calculated in this pass than treat the current point as
+            a suitable candidate;
+        2.5 Move on to the next point until the end of the input dataset has
+            been reached. The last suitable candidate identified is to be
+            saved at the beginning of the next pass.
+*/
 void dk::calcConvexHull2D(DataContainer& convexHull2D, const DataContainer& dataStore, const DataPoint2D& centroid) noexcept {
-    DataPoint2D lastVertexPoint;
-    DataPoint2D lastVertexVector;
+    DataPoint2D lastGoodPoint;
+    DataPoint2D lastGoodVector;
 
     size_t insertionIndex = 0;
     bool bKeepGoing = true;
 
-    // Make sure it doesn't hit the last point during the first pass
-    // because it's likely to show zero angle relative to the first point.
-    // So it will annihilate all the good non-zero angle cases.
+    // Remember - the first point and last point are the same because
+    // we are trying to identify points that form a closed loop of lines.
+    // So make sure the algo doesn't hit the last point during the first pass
+    // because it will show zero angle relative to the first point and,
+    // therefore cause to discard any good non-zero angle case identified.
     size_t offset = 1;
 
     while(bKeepGoing) {
-        lastVertexPoint = dataStore[insertionIndex];
-        lastVertexVector = lastVertexPoint - centroid;
-        convexHull2D.push_back(lastVertexPoint);
+        //lastVertexPoint = dataStore[insertionIndex];
+
+        // Save the good point identified in the previous run.
+        // Also, calculate its respective vector relative to the centroid.
+        lastGoodPoint = dataStore[insertionIndex];
+        convexHull2D.push_back(lastGoodPoint);
+        lastGoodVector = lastGoodPoint - centroid;
         bKeepGoing = false;
 
         double minAngle{impossiblyLargeAngle};
@@ -94,9 +131,15 @@ void dk::calcConvexHull2D(DataContainer& convexHull2D, const DataContainer& data
             DataPoint2D vertexPoint = dataStore[inx];
             DataPoint2D vertexVector = vertexPoint - centroid;
 
-            DataPoint2D deltaVector = vertexVector - lastVertexVector;
-            double angle = DataPoint2D::calcAngle(deltaVector, lastVertexVector);
+            // Evaluate the delta between this vector and last saved vector.
+            DataPoint2D deltaVector = vertexVector - lastGoodVector;
+            double angle = DataPoint2D::calcAngle(deltaVector, lastGoodVector);
 
+            // A smaller angle will indicate a better alignment of the delta
+            // vector with the last saved vector. 
+            // So let's examine the angle and register it along with the
+            // respective point if the value of the angle is smaller than
+            // before.
             if(angle < minAngle) {
                 minAngle = angle;
                 insertionIndex = inx;
